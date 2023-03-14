3aae08aaf187f0343ff399da14d0ed403879b05f
Yoshinori Kanno <kanno@dohouse.co.jp>
2023-03-14 18:04:03 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+
+function MyButton() {
+  return <button>I'm a button</button>
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Hello there.
+        <br />
+        How do you do?
+      </p>
+    </>
+  )
+}
+
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+}
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  )
+}
+
+const products = [
+  { title: "Cabbage", isFruit: false, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
+]
+
+function ShoppingList() {
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? "magenta" : "darkgreen",
+      }}
+    >
+      {product.title}
+    </li>
+  ))
+
+  return <ul>{listItems}</ul>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ShoppingList />
+        <AboutPage />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +80,11 @@ function App() {
         >
           Learn React
         </a>
+        <MyButton />
+        <Profile />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
